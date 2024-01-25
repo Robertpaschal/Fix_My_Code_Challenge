2792fc32570ddc3be3658a867763eb0753f9639e
Odinaka <nnamani.odinakarobert@gmail.com>
2024-01-25 01:58:04 -0800

    doubly linked list

--- 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -22,12 +22,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = NULL;
+	new->prev = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
-		new->prev = NULL;
 		return (new);
 	}
+
 	l = *head;
 	while (l->next != NULL)
 	{
