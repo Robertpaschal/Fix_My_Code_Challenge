423336ab744e4cdd9e1c01b09c558ab7016c7631
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-21 11:38:09 +0100

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -19,9 +19,9 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def Permiter_Of_My_Square(self):
+    def Permiter_of_my_square(self):
         """
-        Perimeter of the square
+        Perimiter of the square
         """
         return (self.width * 2) + (self.height * 2)
 
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.Permiter_Of_My_Square())
+    print(s.Permiter_of_my_square())
